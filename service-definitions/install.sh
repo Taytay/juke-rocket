bcf29a3ebf3bb1edf2c28df2e60b588bc9b442b8
Taylor Brown <taytay@taytay.com>
2022-11-18 15:37:30 -0600

    Remove old install.sh script

--- service-definitions/install.sh
+++
@@ -1,5 +0,0 @@
-# Installs the spotifyd service:
-sudo cp ./spotifyd.service /etc/systemd/system/spotifyd.service
-sudo chown root:root /etc/systemd/system/spotifyd.service
-sudo chmod 644 /etc/systemd/system/spotifyd.service
-sudo systemctl enable spotifyd
